287f251c6c253622d27c113a0c9074d544cd90e3
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2022-04-11 10:58:34 -0500

    [ADD] General Project

---
+++ Assets/Scripts/BossMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour
+{
+
+    [Header("Boss Speed")]
+    public float speed = 0.5f;
+    
+    [Header("Boss Targets")]
+    public List<GameObject> checkPoints = new List<GameObject>();
+    public int randormIdx;
+    public List<GameObject> currentCheckPoints = new List<GameObject>();
+
+    [Header("Current Target")]
+    public GameObject currentTarget;
+
+    void Start()
+    {
+	GetCheckPoint();
+    }
+ 
+     void Update()
+     {
+	// Speed of the Boss
+	float step = speed * Time.deltaTime;
+
+	// Go to the target
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget.transform.position, step);
+
+	// Change the target
+	if(transform.position == currentTarget.transform.position) {
+		GetCheckPoint();
+	} 
+
+
+     }
+
+     void GetCheckPoint() {
+	// Init with a random position
+	randormIdx = Random.Range(0, checkPoints.Count);
+	currentTarget = checkPoints[randormIdx];
+	currentCheckPoints.Add(currentTarget);
+	checkPoints.Remove(currentTarget);
+     }
+}
---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public GameObject Particles;
+
+    void OnTriggerEnter(Collider col)
+    {
+	Debug.Log("Hubo Colisión 1");
+	if(col.gameObject.tag == "Boss"){
+	   Debug.Log("Generar Particulas");
+           Particles.SetActive(true);
+	} else if (col.gameObject.tag == "Player"){
+	   Debug.Log("Detener Particulas");
+           Particles.SetActive(false);
+	}
+        
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+	//Debug.Log("Hubo Colisión 2");
+        
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+	Debug.Log("Hubo Colisión 3");
+        
+    }
+}
