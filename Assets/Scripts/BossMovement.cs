0eaee4ec3635d706a4fe24a3587961ba681fb945
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2022-04-11 13:14:39 -0500

    [ADD] Boss config scene1

--- Assets/Scripts/BossMovement.cs
+++ Assets/Scripts/BossMovement.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class BossMovement : MonoBehaviour
 {
 
-    [Header("Boss Speed")]
+    [Header("Boss Info")]
     public float speed = 0.5f;
+    public bool canMove;
     
     [Header("Boss Targets")]
     public List<GameObject> checkPoints = new List<GameObject>();
@@ -16,32 +17,53 @@ public class BossMovement : MonoBehaviour
     [Header("Current Target")]
     public GameObject currentTarget;
 
+     [Header("Boss WaitTime")]
+    public float waitTime = 1f;
+
     void Start()
     {
-	GetCheckPoint();
+         GetCheckPoint();
+         canMove = true;
     }
  
      void Update()
      {
-	// Speed of the Boss
-	float step = speed * Time.deltaTime;
-
-	// Go to the target
-        transform.position = Vector3.MoveTowards(transform.position, currentTarget.transform.position, step);
-
-	// Change the target
-	if(transform.position == currentTarget.transform.position) {
-		GetCheckPoint();
-	} 
+          // Speed of the Boss
+	     float step = speed * Time.deltaTime;
 
 
+          if(canMove) {
+               // Go to the target
+               transform.position = Vector3.MoveTowards(transform.position, currentTarget.transform.position, step);
+               //transform.rotation = Quaternion.RotateTowards(transform.rotation, currentTarget.transform.rotation, step);
+          }
+
+          // Change the target
+          if(transform.position == currentTarget.transform.position) {
+               StartCoroutine(WaitDestroyPC());
+               GetCheckPoint();
+          } 
      }
 
      void GetCheckPoint() {
 	// Init with a random position
-	randormIdx = Random.Range(0, checkPoints.Count);
-	currentTarget = checkPoints[randormIdx];
-	currentCheckPoints.Add(currentTarget);
-	checkPoints.Remove(currentTarget);
+          randormIdx = Random.Range(0, checkPoints.Count);
+          currentTarget = checkPoints[randormIdx];
+          currentCheckPoints.Add(currentTarget);
+          checkPoints.Remove(currentTarget);
      }
+
+     IEnumerator WaitDestroyPC()
+    {
+        //Print the time of when the function is first called.
+        canMove = false;
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(waitTime);
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+        canMove = true;
+    }
 }
