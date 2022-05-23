f3d03a7adbc7e68f95f1726011db1a49dca94e51
juanjosbg <juanjoseborrero95@gmail.com>
2022-05-23 01:26:53 -0500

    personaje con animación de reparar ready

---
+++ Assets/Scripts/Reapir.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reapir : MonoBehaviour
+{
+
+    public Animator Anim;
+    public float repairSpeed;
+
+    // Start is called before the first frame update
+    void Start(){}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.P))
+        {
+            Anim.SetBool("Rep", true);
+            GameObject.Find("Personaje1").GetComponent<LogicaPersonaje1>().enabled = false;
+            transform.Translate(Vector3.forward * repairSpeed * Time.deltaTime);
+        }   
+        else
+        {
+            Anim.SetBool("Rep", false);
+            GameObject.Find("Personaje1").GetComponent<LogicaPersonaje1>().enabled = true;
+        }
+    }
+}
