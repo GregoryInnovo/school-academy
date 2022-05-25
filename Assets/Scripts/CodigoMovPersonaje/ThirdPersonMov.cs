47c7dfa7d30ca80e3abd68ec35004636e76cd8ea
RiascosJC <juan_camilo.riascos@uao.edu.co>
2022-05-24 23:20:43 -0500

    Posible borrado
    revsando el pull que no se hizo

---
+++ Assets/Scripts/CodigoMovPersonaje/ThirdPersonMov.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMov : MonoBehaviour
+{
+   public CharacterController controller;
+
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVel;
+
+   void Update()
+   {
+       float Horizontal = Input.GetAxisRaw("Horizontal");
+       float Vertical = Input.GetAxisRaw("Vertical");
+       Vector3 direction = new Vector3(Horizontal, 0f, Vertical).normalized;  
+
+       if(direction.magnitude >= 0.1f)
+       {
+           float targetAngle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+           float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVel, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+           controller.Move(direction * speed * Time.deltaTime);
+       }  
+   }
+}
---
+++ Assets/Scripts/CodigoPostPartida/Nota.cs
@@ -0,0 +1,23 @@
+/* using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nota : MonoBehaviour
+{
+    public int Nota;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(time >= 1:45)
+        Nota = 4.0;      
+    }else if(time > 1:00 && time < 1:45)
+    {
+        Nota = 3.5;
+    }else (time < 1:00)
+    {
+        Nota = 2.0;
+    }
+
+
+
+} */
